f66534aa6f1765ea6faca2b1328b944164bde06d
Stuart Mumford <stuart@cadair.com>
2024-04-16 10:42:37 +0100

    Fix race / sprint race confusion

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
-import ics
-from urllib.request import urlopen
 import datetime
-from opsdroid.matchers import match_regex
-from opsdroid.skill import Skill
-from cachetools import TTLCache
-import pytz
 from textwrap import dedent
+from urllib.request import urlopen
+import logging
 
+import ics
+import pytz
+from cachetools import TTLCache
+from opsdroid.matchers import match_regex, match_crontab
+from opsdroid.skill import Skill
 
 F1BOT_COMMAND_PREFIX = "!"
 F1BOT_COMMANDS = {}
+
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def regex_command(command, description="", friendly_command=None, **kwargs):
@@ -56,12 +60,16 @@ class Formula1Events(Skill):
     def get_all_events(self):
         return list(self.cal.timeline.start_after(datetime.datetime.now(self.tz)))
 
-    def get_next_event(self, session="race"):
-        events = list(filter(lambda e: session in e.name.lower(), self.get_all_events()))
+    def get_next_event(self, session=None):
+        events = self.get_all_events()
+        if session is not None:
+            events = list(filter(lambda e: session in e.name.lower(), events))
+            # sprint races are stupid
+            if session == "race":
+                events = list(filter(lambda e: "sprint" not in e.name.lower(), events))
         return events[0]
 
     def next_event_info(self, session=None, display_tz=pytz.UTC):
-        session = session or "race"
         event = self.get_next_event(session=session)
         time_fmt = "%Y-%m-%d %H:%M:%S %Z"
         start = event.begin.astimezone(display_tz).strftime(time_fmt)
@@ -76,9 +84,9 @@ class Formula1Events(Skill):
         return list(f1.cal.timeline.included(now,
                                              now+datetime.timedelta(minutes=timedelta)))
 
-    @regex_command("next\s?(?P<session>(?:race|quali|practice))?\s?(?P<tz>.*)",
+    @regex_command("next\s?(?P<session>(?:race|quali|sprint|practice|any))?\s?(?P<tz>.*)",
                    "Get the information about the next F1 event.",
-                   friendly_command="next [race|quali|practice] [timezone]")
+                   friendly_command="next [race|quali|sprint|practice|any] [timezone]")
     async def next_event_command(self, message):
         tz = message.entities['tz']['value']
         if not tz:
@@ -88,6 +96,10 @@ class Formula1Events(Skill):
         except pytz.UnknownTimeZoneError:
             return await message.respond(f"Unable to parse timezone {tz}")
         session = message.entities['session']['value']
+        if session is None:
+            session = "race"
+        if session == "any":
+            session = None
         await message.respond(self.next_event_info(session=session, display_tz=tz))
 
     @regex_command("settz\s?(?P<tz>.*)",
